49f67bb15886da94b9ddc4b998771b50f911d09b
Ankie <ankie555@hotmail.com>
2021-12-02 19:50:05 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿var input = File.ReadAllLines(@"Input.txt");
+var measures = input.Select(c => int.Parse(c.ToString())).ToList();
+
+Part1();
+Part2();
+
+void Part1()
+{
+    var lastMeasure = measures.First();
+    var increasesCount = 0;
+    foreach (var measure in measures)
+    {
+        if (measure > lastMeasure)
+        {
+            increasesCount++;
+        }
+        lastMeasure = measure;
+    }
+
+    Console.WriteLine($"Number of increases: {increasesCount}"); // 1195
+}
+
+void Part2()
+{
+    var lastSumOfMeasures = measures[0] + measures[1] + measures[2];
+    var increasesSumCount = 0;
+    for (var index = 0; index < measures.Count-2; index++)
+    {
+        var sumOfMeasures = measures[index] + measures[index+1] + measures[index+2];
+        if (sumOfMeasures > lastSumOfMeasures)
+        {
+            increasesSumCount++;
+        }
+
+        lastSumOfMeasures = sumOfMeasures;
+    }
+
+    Console.WriteLine($"Number of sum increases: {increasesSumCount}"); // 1235
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,63 @@
+﻿var input = File.ReadAllLines(@"Input.txt");
+var steps = input.Select(line => new Step(line.Split(' ')[0], 
+    int.Parse(line.Split(' ')[1]))).ToList();
+
+Part1();
+Part2();
+
+void Part1()
+{
+    var pos = new Point(0,0);
+    foreach (var (action, amount) in steps)
+    {
+        switch (action)
+        {
+            case "forward":
+                pos.X += amount;
+                break;
+            case "up":
+                pos.Y -= amount;
+                break;
+            case "down":
+                pos.Y += amount;
+                break;
+        }
+    }
+    Console.WriteLine($"Position X:{pos.X}, Y:{pos.Y}");
+    Console.WriteLine($"Multiply X and Y:{pos.X} x {pos.Y} = {pos.X * pos.Y}"); // 1989265
+}
+
+void Part2()
+{
+    var pos = new Point(0, 0);
+    var aim = 0;
+    foreach (var (action, amount) in steps)
+    {
+        switch (action)
+        {
+            case "forward":
+                pos.X += amount;
+                pos.Y += aim * amount;
+                break;
+            case "up":
+                aim -= amount;
+                break;
+            case "down":
+                aim += amount;
+                break;
+        }
+    }
+    Console.WriteLine($"Position X:{pos.X}, Y:{pos.Y}");
+    Console.WriteLine($"Multiply X and Y:{pos.X} x {pos.Y} = {pos.X * pos.Y}"); // 2089174012
+}
+
+public record Step(string Action, int Amount)
+{
+    public string Action { get; } = Action;
+    public int Amount { get; } = Amount;
+}
+public record Point(int X, int Y)
+{
+    public int X { get; set; } = X;
+    public int Y { get; set; } = Y;
+}
